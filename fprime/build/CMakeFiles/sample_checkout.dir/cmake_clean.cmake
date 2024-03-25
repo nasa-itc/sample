398f303ad50c38b8260fd115a4a046d87a33824b
Lynch <zlynch@ndc.nasa.gov>
2024-03-25 17:02:32 -0400

    [nasa/nos3#84] fprime sample app framework

---
+++ fprime/build/CMakeFiles/sample_checkout.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sample_checkout.dir/home/jstar/Desktop/github-nos3/components/sample/fsw/src/sample_device.c.o"
+  "CMakeFiles/sample_checkout.dir/home/jstar/Desktop/github-nos3/components/sample/fsw/src/sample_device.c.o.d"
+  "CMakeFiles/sample_checkout.dir/home/jstar/Desktop/github-nos3/fsw/apps/hwlib/sim/src/libcan.c.o"
+  "CMakeFiles/sample_checkout.dir/home/jstar/Desktop/github-nos3/fsw/apps/hwlib/sim/src/libcan.c.o.d"
+  "CMakeFiles/sample_checkout.dir/home/jstar/Desktop/github-nos3/fsw/apps/hwlib/sim/src/libi2c.c.o"
+  "CMakeFiles/sample_checkout.dir/home/jstar/Desktop/github-nos3/fsw/apps/hwlib/sim/src/libi2c.c.o.d"
+  "CMakeFiles/sample_checkout.dir/home/jstar/Desktop/github-nos3/fsw/apps/hwlib/sim/src/libspi.c.o"
+  "CMakeFiles/sample_checkout.dir/home/jstar/Desktop/github-nos3/fsw/apps/hwlib/sim/src/libspi.c.o.d"
+  "CMakeFiles/sample_checkout.dir/home/jstar/Desktop/github-nos3/fsw/apps/hwlib/sim/src/libuart.c.o"
+  "CMakeFiles/sample_checkout.dir/home/jstar/Desktop/github-nos3/fsw/apps/hwlib/sim/src/libuart.c.o.d"
+  "CMakeFiles/sample_checkout.dir/home/jstar/Desktop/github-nos3/fsw/apps/hwlib/sim/src/nos_link.c.o"
+  "CMakeFiles/sample_checkout.dir/home/jstar/Desktop/github-nos3/fsw/apps/hwlib/sim/src/nos_link.c.o.d"
+  "CMakeFiles/sample_checkout.dir/test.cpp.o"
+  "CMakeFiles/sample_checkout.dir/test.cpp.o.d"
+  "sample_checkout"
+  "sample_checkout.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/sample_checkout.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
