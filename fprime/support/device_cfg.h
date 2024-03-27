de9d856f844fb705b7f4b401988d7380fbe3221f
Lucas, John P <John.P.Lucas@nasa.gov>
2024-03-27 16:30:51 -0400

    [nasa/nos3#84] fprime sample checkout framework;

--- fprime/support/device_cfg.h
+++ fprime/support/device_cfg.h
@@ -6,8 +6,8 @@
 */
 #define SAMPLE_CFG
 /* Note: NOS3 uart requires matching handle and bus number */
-#define SAMPLE_CFG_STRING           "/dev/usart_16"
-#define SAMPLE_CFG_HANDLE           16 
+#define SAMPLE_CFG_STRING           "/dev/usart_29"
+#define SAMPLE_CFG_HANDLE           29 
 #define SAMPLE_CFG_BAUDRATE_HZ      115200
 #define SAMPLE_CFG_MS_TIMEOUT       250
 #define SAMPLE_CFG_DEBUG
