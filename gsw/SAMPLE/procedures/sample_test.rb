387b3f21fe5a1c86d432664cbc5bc14c45a9df56
Kevin Bruce <kevin.c.bruce@nasa.gov>
2025-03-18 17:26:58 -0400

    [nasa/nos3#228] Started to build basic sample system test suite

---
+++ gsw/SAMPLE/procedures/sample_test.rb
@@ -0,0 +1,45 @@
+require 'cosmos'
+require 'cosmos/script'
+require 'mission_lib.rb'
+
+class LPT < Cosmos::Test
+  def setup
+      enable_TO_and_verify()
+  end
+
+  def test_lpt
+      start("tests/sample_lpt_test.rb")
+  end
+
+  def teardown
+      cmd("CFS_RADIO TO_PAUSE_OUTPUT")
+  end
+end
+
+class CPT < Cosmos::Test
+  def setup
+      
+  end
+
+  def test_cpt
+      start("tests/sample_cpt_test.rb")
+  end
+
+  def teardown
+
+  end
+end
+
+class Sample_Test < Cosmos::TestSuite
+  def initialize
+      super()
+      add_test('CPT')
+      add_test('LPT')
+  end
+
+  def setup
+  end
+  
+  def teardown
+  end
+end
