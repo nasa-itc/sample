42f8f783d14129830eddebb16d5679a4b010ef60
Mark Suder <mark.d.suder@nasa.gov>
2025-04-15 15:59:05 -0400

    https://github.com/nasa/nos3/issues/571 - Add more coverage tests for sample_device.c functions.

---
+++ fsw/cfs/unit-test/coveragetest/coveragetest_sample_device.c
@@ -0,0 +1,68 @@
+
+#include "sample_app_coveragetest_common.h"
+
+void Test_SAMPLE_ReadData(void)
+{
+    uart_info_t device;
+    uint8_t read_data[8];
+    uint8_t data_length = 8;
+    SAMPLE_ReadData(&device, read_data, data_length);
+
+    UT_SetDeferredRetcode(UT_KEY(uart_bytes_available), 1, data_length);
+    SAMPLE_ReadData(&device, read_data, data_length);
+
+    UT_SetDeferredRetcode(UT_KEY(uart_bytes_available), 1, data_length+1);
+    SAMPLE_ReadData(&device, read_data, data_length);
+}
+
+void Test_SAMPLE_CommandDevice(void)
+{
+    uart_info_t device;
+    uint8_t cmd_code = 0;
+    uint32_t payload = 0;
+    SAMPLE_CommandDevice(&device, cmd_code, payload);
+
+    UT_SetDeferredRetcode(UT_KEY(uart_write_port), 1, SAMPLE_DEVICE_CMD_SIZE);
+    SAMPLE_CommandDevice(&device, cmd_code, payload);    
+}
+
+void Test_SAMPLE_RequestHK(void)
+{
+    uart_info_t device;
+    SAMPLE_Device_HK_tlm_t data;
+    SAMPLE_RequestHK(&device, &data);
+
+    UT_SetDefaultReturnValue(UT_KEY(SAMPLE_ReadData), OS_SUCCESS);
+    SAMPLE_RequestHK(&device, &data);    
+}
+
+void Test_SAMPLE_RequestData(void)
+{
+    uart_info_t device;
+    SAMPLE_Device_Data_tlm_t data;
+    SAMPLE_RequestData(&device, &data);
+}
+
+/*
+ * Setup function prior to every test
+ */
+void Sample_UT_Setup(void)
+{
+    UT_ResetState(0);
+}
+
+/*
+ * Teardown function after every test
+ */
+void Sample_UT_TearDown(void) {}
+
+/*
+ * Register the test cases to execute with the unit test tool
+ */
+void UtTest_Setup(void)
+{
+    ADD_TEST(SAMPLE_ReadData);
+    ADD_TEST(SAMPLE_CommandDevice);
+    ADD_TEST(SAMPLE_RequestHK);
+    ADD_TEST(SAMPLE_RequestData);
+}
